f16db336556582127f421a8d1a039c401427bc13
Oscar Triano dotoscat <user@example.com>
2017-05-29 23:12:33 +0200

    Base class for server

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
 print("Hola mundo")
+class Server(DatagramProtocol):
+    pass
