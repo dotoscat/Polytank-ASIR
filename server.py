6ab550fcb43c91d479c5d68c3d51baf09c337242
Oscar Triano dotoscat <user@example.com>
2017-05-30 18:15:19 +0200

    Display formated data

--- server.py
+++ server.py
@@ -3,11 +3,10 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
-print("Hola mundo")
 class Server(DatagramProtocol):
     
     def datagramReceived(self, data, addr):
-        print("{} from {}".format(data, addr))
+        print("{} from {}".format(str(data, "utf8"), addr))
 
 if __name__ == "__main__":
     reactor.listenUDP(7777, Server())
