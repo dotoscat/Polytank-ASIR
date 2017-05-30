6718077d29b46e1df787fe53d7a4ddb5bd4f6998
Oscar Triano dotoscat <user@example.com>
2017-05-30 18:15:41 +0200

    Add client

---
+++ client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+class Client(DatagramProtocol):
+    
+    def startProtocol(self):
+        host = "127.0.0.1"
+        port = 7777
+        
+        self.transport.connect(host, port)
+        self.transport.write(b'hello server! :D')
+        
+    def datagramReceived(self):
+        pass
+        
+    def connectionRefused(self):
+        print("With hope some server will be listening")
+        reactor.stop()
+        
+if __name__ == "__main__":
+    reactor.listenUDP(0, Client())
+    reactor.run()
+        
