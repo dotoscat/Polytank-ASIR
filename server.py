610e3e7f38db680b601db4f17a4cc2147f391e3d
Oscar Triano dotoscat <user@example.com>
2017-05-31 16:20:41 +0200

    Add Engine

--- server.py
+++ server.py
@@ -3,7 +3,14 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+class Engine(object):
+    def __init__(self):
+        self.entities = []
+
 class Server(DatagramProtocol):
+    def __init__(self):
+        super(Server, self).__init__()
+        self.engine = Engine()
     
     def datagramReceived(self, data, addr):
         print("{} from {}".format(str(data, "utf8"), addr))
