e2ec3241a3becf19954e287a6f9caba3ba445829
Oscar Triano dotoscat <user@example.com>
2017-06-16 15:58:32 +0200

    Add flags for protocol

--- polytanks/protocol.py
+++ polytanks/protocol.py
@@ -15,20 +15,37 @@
 
 import struct
 
-CONNECT = 0
-DISCONNECT = 1
-RECREATE_TANK = 2
-MOVE = 3
+#COMMANDS
+CONNECT = 0x0001
+DISCONNECT = 0x0002
+INPUT = 0x0003
+SNAPSHOT = 0x0004
 
-_command = struct.Struct("!i")
+#INPUT
+MOVE_LEFT = 0x0100 << 1
+MOVE_RIGHT = 0x0100 << 2
+PRESS_SHOT = 0x0100 << 3
+RELEASE_SHOT = 0x0100 << 4
+JUMP = 0x0100 << 5
+AIM = 0x0100 << 6
+
+#OBJECT TYPE
+TANK = 0x0100
+
+_buffer = bytearray(64)
+
+_command = struct.Struct("!xi")
 _recreate_tank = struct.Struct("!iiff")
 _move = struct.Struct("!iif")
+
+def command_is(command, data):
+    return _command.unpack_from(data)[0] & command == command
 
 def get_command(data):
     return _command.unpack_from(data)[0]
 
 def connect():
-    return _command.pack(CONNECT)
+    return _command.pack_into(_buffer, 0, CONNECT)
 
 def move(id_, direction):
     return _move.pack(MOVE, id_, direction)
