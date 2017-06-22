0bdec6eb209c08347f32c0c26400769e7c65368f
Oscar Triano dotoscat <user@example.com>
2017-06-22 13:34:49 +0200

    Call engine method based on Body type

--- polytanks/protocol.py
+++ polytanks/protocol.py
@@ -14,6 +14,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import struct
+from inspect import getargspec
 from . import engine
 
 #COMMANDS
@@ -71,6 +72,7 @@ def set_engine_from_snapshot(the_engine, buffer_):
     position = _command.size
     buffer_len = len(buffer_)
     while position < buffer_len:
+        print(position, buffer_len)
         object_type = _object_type.unpack_from(buffer_, position)[0]
         print('object_type', object_type)
         #You don't increase position because object_Type is part of Body
@@ -81,7 +83,10 @@ def set_engine_from_snapshot(the_engine, buffer_):
             values.extend(type_struct.unpack_from(buffer_, position))
             position += type_struct.size
         action = _actions[object_type]
-        print(action)
+        engine_method = getattr(the_engine, action)
+        method_spec = getargspec(engine_method)
+        n_args = len(method_spec.args) - 1
+        engine_method(*values[2:2+n_args])
 
 def move(id_, direction):
     return _move.pack(MOVE, id_, direction)
