f4c8ecf8f52e114cac324d40dbd8678aca47e750
Oscar Triano dotoscat <user@example.com>
2017-10-13 18:19:49 +0200

    Quitar importacion no usada

--- polytanks/constant.py
+++ polytanks/constant.py
@@ -13,8 +13,6 @@
 #You should have received a copy of the GNU Affero General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import assets
-
 TANK = 1
 PLATFORM = 1 << 1
 BULLET = 1 << 2
