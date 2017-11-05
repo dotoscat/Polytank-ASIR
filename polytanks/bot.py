6d6e4ddbb78239b3907a0d12a76e82d28d9eb315
Oscar Triano dotoscat <user@example.com>
2017-11-05 07:34:48 +0100

    Bots!

---
+++ polytanks/bot.py
@@ -0,0 +1,28 @@
+#Copyright (C) 2017  Oscar Triano 'dotoscat' <dotoscat (at) gmail (dot) com>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+    Bots are callables with the following signature
+    
+    brain(world, entity) -> None:
+    
+    World and part of entity is an input.
+    entity.input is the output.
+    A bot can do nothing
+"""
+
+def jumper(world, entity):
+    if entity.platform.touch_floor:
+        entity.input.jump()
