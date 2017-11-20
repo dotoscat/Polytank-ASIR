be8a5931617d5ad6b534eddbe2612f447b63b764
Oscar Triano dotoscat <user@example.com>
2017-11-20 20:48:20 +0100

    Disparar en el servidor si cliente suelta el boton de disparo

--- polytanks/server.py
+++ polytanks/server.py
@@ -129,6 +129,8 @@ class Server(asyncio.DatagramProtocol):
         move, cannon_angle, accumulate_power, do_jump = player_input
         tank.input.move = move
         tank.input.cannon_angle = cannon_angle
+        if not accumulate_power and tank.input.accumulate_power:
+            tank.input.shoots = True 
         tank.input.accumulate_power = accumulate_power
         tank.input.do_jump = do_jump
         #print("input", addr, tick, move, cannon_angle, shoots,
