5533ab0e31df4c5738d32bff69e1dc0c12d2763a
Oscar Triano dotoscat <user@example.com>
2017-11-30 21:51:21 +0100

    Crear una ventana basica

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env  python
+#Copyright (C) 2017  Oscar Triano 'dotoscat' <dotoscat (at) gmail (dot) com>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyglet
+from polytanks import constant
+import polytanks.ogf4py3 as ogf4py3
+
+if __name__ == "__main__":
+    director = ogf4py3.Director(
+        caption="Polytanks", fullscreen=False,
+        width=constant.WIDTH, height=constant.HEIGHT,
+        vwidth=constant.VWIDTH, vheight=constant.VHEIGHT)
+    director.set_background_color(0., 0., 0.)
+    pyglet.app.run()
+    
