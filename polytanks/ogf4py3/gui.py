d16d0f2ead4326f9291007effe737b689f887f9c
Oscar Triano dotoscat <user@example.com>
2017-12-03 21:15:29 +0100

    Eliminar Rectangle

--- polytanks/ogf4py3/gui.py
+++ polytanks/ogf4py3/gui.py
@@ -237,14 +237,6 @@ class Button(VisibleLabel):
         and self.y < y < self.y + self.__height):
             self.action(self, x, y, button, modifiers)
 
-class Rectangle(object):
-    '''Draws a rectangle into a batch.'''
-    def __init__(self, x1, y1, x2, y2, batch):
-        self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, None,
-            ('v2i', [x1, y1, x2, y1, x2, y2, x1, y2]),
-            ('c4B', [200, 200, 220, 128] * 4)
-        )
-
 class TextEntry(pyglet.text.layout.IncrementalTextLayout):
     def __init__(self, width, height, text="", **kwargs):
 
